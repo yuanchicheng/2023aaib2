7ad43c91e29a1b83123fb36a05a147dccd65f618
yuanchicheng <kittycheng1212@gmail.com>
2024-03-15 12:02:14 +0800

    week04

---
+++ week04/week04-3.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    for(int i=1; i<=9; i++){
+        printf("i¬O%d" ,i);
+    }
+    printf("\n");
+        for(int i=9; i>=1; i--){
+            printf("i¬O%d" ,i);
+        }
+}
